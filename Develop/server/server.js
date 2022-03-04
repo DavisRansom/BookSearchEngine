93a8df58179ab130865c219908292a94b79177a0
DavisRansom <davisransom.vb@gmail.com>
2022-03-04 15:11:23 -0800

    adding graphql support for resolver and server

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-
-const path = require('path');
+const { ApolloServer } = require('apollo-server-express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
